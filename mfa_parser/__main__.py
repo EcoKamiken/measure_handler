c21c120eee017164e1db271b575f8b44e3631c44
Nakajima Kazuki <nakajima.k0301@gmail.com>
2019-12-24 12:49:57 +0900

    フォルダ構造作成

---
+++ mfa_parser/__main__.py
@@ -0,0 +1,4 @@
+import mfa_parser.hello
+
+if __name__ == '__main__':
+    mfa_parser.hello.hello()
---
+++ tests/test_hello.py
@@ -0,0 +1,5 @@
+from mfa_parser.hello import hello
+
+
+def test_hello():
+    assert hello() == 'hello'
